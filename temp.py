29f790f2274f441ef234120123ee4fea7340c47c
Moe Fwacky <moe.fwacky@gmail.com>
2018-02-17 10:06:39 -0800

    Update temp.py
    This branch is made to work on a Pi running RasPlex/openelec

--- temp.py
+++ temp.py
@@ -1,14 +1,16 @@
 #! /usr/bin/python
 import os
 import time
+import sys
+sys.path.append('/storage/lib')
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-PWR =15
+#PWR =15
 HOT =5
 NOT =13
-GPIO.setup(PWR, GPIO.OUT)
+#GPIO.setup(PWR, GPIO.OUT)
 
 GPIO.setup(HOT, GPIO.OUT)
 HOT_LED = GPIO.PWM( HOT, 50)
@@ -16,7 +18,7 @@ HOT_LED = GPIO.PWM( HOT, 50)
 GPIO.setup(NOT, GPIO.OUT)
 NOT_LED = GPIO.PWM( NOT, 50)
 
-GPIO.output(PWR,True)
+#GPIO.output(PWR,True)
 temperature = os.popen("cat /sys/class/thermal/thermal_zone0/temp").readline()
 i = int(temperature)
 s = str(i)
@@ -24,14 +26,12 @@ blue = int(s[0])
 red = int(s[1])
 
 def blink_blue():
-#               print(blue)
                 time.sleep(.1)
                 NOT_LED.start(1)
                 time.sleep(.1)
                 NOT_LED.start(0)
 
 def blink_red():
-#               print(red)
                 time.sleep(.1)
                 HOT_LED.start(1)
                 time.sleep(.1)
